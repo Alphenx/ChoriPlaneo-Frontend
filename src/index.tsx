41ab8639f7a0d64b3d6a550652d6d2abf17085e5
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-16 17:30:24 +0100

    Add Loading, models and variables.css to index

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import './styles/fonts.css';
-import './styles/theme.css';
+import './styles/variables.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
