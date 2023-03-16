11945a7903560eb66e2c52b8176ef70aeab4e283
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-16 17:25:26 +0100

    Modify MainLayout styles

--- src/layout/MainLayout/MainLayoutStyled.ts
+++ src/layout/MainLayout/MainLayoutStyled.ts
@@ -2,10 +2,5 @@ import styled from 'styled-components';
 
 export const MainLayoutStyled = styled.div`
   font-family: var(--main-font-regular);
-  max-width: 1440px;
-  margin: 15rem auto;
-
-  @media (max-width: 426px) {
-    margin: 5rem auto;
-  }
+  margin: 0 auto;
 `;
