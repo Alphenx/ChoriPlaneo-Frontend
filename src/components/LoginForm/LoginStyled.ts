cba67b78258750245ee29ac8042631299043a874
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-17 20:09:45 +0100

    Add LoginForm component with tests and styles

---
+++ src/components/LoginForm/LoginStyled.ts
@@ -0,0 +1,120 @@
+import styled from 'styled-components';
+import { AuthStatus } from '../../shared/models/api-status';
+// import { RegisterStatus } from '../../shared/models/api-status';
+
+export const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1rem;
+  gap: 1rem;
+
+  div {
+    width: 100%;
+    height: 2rem;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: 0.2rem;
+    font-family: var(--main-font-regular);
+    font-size: var(--font-size-xxs);
+    background-color: var(--base-color-bg);
+    position: relative;
+
+    :has(input:focus) {
+      color: var(--main-color);
+    }
+
+    :has(:focus:required:invalid, .notMatch:not(:focus)) {
+      color: var(--red-color);
+    }
+
+    span {
+      margin-left: 10px;
+      top: -8px;
+      padding: 0 4px;
+      position: absolute;
+      background-color: var(--base-color-bg);
+      width: fit-content;
+    }
+
+    .notMatch {
+      outline: var(--border-m) var(--red-color);
+      color: var(--red-color);
+    }
+
+    input {
+      color: var(--base-color-contrast);
+      width: 100%;
+      height: 2.6rem;
+      border: var(--border-s) var(--base-color-contrast);
+      background-color: var(--base-color-bg);
+      border-radius: var(--radius-xs);
+      padding: 0.5rem;
+      :focus {
+        border: none;
+        outline: var(--border-m) var(--main-color);
+        color: var(--main-color);
+      }
+
+      :invalid:focus {
+        outline: var(--border-m) var(--red-color);
+        color: var(--red-color);
+      }
+    }
+  }
+
+  p {
+    text-align: center;
+    font-family: var(--main-font-light);
+    font-size: var(--font-size-xxs);
+    padding: 0rem 1.5rem;
+  }
+
+  .login-link {
+    font-family: var(--main-font-light);
+    font-size: var(--font-size-s);
+    margin-bottom: 1rem;
+
+    a {
+      color: var(--main-color);
+      font-family: var(--main-font-bold);
+    }
+  }
+`;
+
+export const LoginButtonStyled = styled.button`
+  font-family: var(--main-font-regular);
+  font-size: var(--font-size-s);
+  background-color: var(--main-color);
+  color: var(--base-color-text-light);
+  border: none;
+  border-radius: var(--radius-l);
+  padding: 1rem;
+  :hover {
+    cursor: pointer;
+    scale: 1.02;
+  }
+`;
+
+interface AuthStatusProps {
+  authStatus: AuthStatus.SUCCESS | AuthStatus.LOADING | AuthStatus.ERROR;
+}
+
+export const FeedBackStyled = styled.h3<AuthStatusProps>`
+  background-color: ${props =>
+    props.authStatus === AuthStatus.SUCCESS
+      ? 'var(--green-color)'
+      : 'var(--red-color)'};
+
+  font-family: var(--main-font-light);
+  font-size: var(--font-size-xs);
+  color: var(--base-color-text-light);
+
+  border: none;
+  border-radius: var(--radius-l);
+  text-align: center;
+  padding: 0.3rem;
+`;
