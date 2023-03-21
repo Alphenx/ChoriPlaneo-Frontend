ea5ae6c47056dd3d419ac33bba2cd4b0a3570d6a
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-21 20:45:15 +0100

    Add AllPlansCardList to Homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import AllPlansCardList from '../../components/AllPlansCardList/AllPlansCardList';
 
-const HomeSectionStyled = styled.section`
-  text-align: center;
-  width: 100%;
-  height: 500px;
-
-  background-image: url('https://miro.medium.com/v2/resize:fit:720/format:webp/1*y7ZgcnbxdF4aDgh-XaS1-Q.png');
-  background-position: center;
-  background-repeat: no-repeat;
-
-  h1 {
-    font-family: var(--main-font-bold);
-    font-size: var(--font-size-xl);
-  }
-`;
+const HomeSectionStyled = styled.section``;
 
 const Home = () => {
   return (
     <HomeSectionStyled>
-      <h1>Choriplaneo is coming soon!</h1>
+      <AllPlansCardList />
     </HomeSectionStyled>
   );
 };
