adcf5728af41c91fc1dffca0e4fe46b2e8e77018
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-29 00:42:38 +0200

    Add Card variables for detail page

--- src/shared/components/Card/Card.tsx
+++ src/shared/components/Card/Card.tsx
@@ -3,22 +3,34 @@ import { Plan } from '../../../features/plans/plan.model';
 import { CardStyled } from './CardStyled';
 import * as AiIcons from 'react-icons/ai';
 import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 interface CardProps {
   plan: Plan;
-  cardType: 'public' | 'myplans' | 'recommended';
+  cardType: string;
+  detail?: boolean;
 }
 
-const Card: FC<CardProps> = ({ plan, cardType }) => {
+const Card: FC<CardProps> = ({ plan, cardType, detail = false }) => {
+  const date = new Date(plan.date);
+  const dateString = date.toLocaleString('en', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hourCycle: 'h12',
+  });
+
   return (
-    <CardStyled cardType={cardType}>
+    <CardStyled cardType={cardType} detail={detail}>
       <div className="top-label">
         <div className="creator-info" data-testid="creator-info">
           <img
             src={
               plan.creator?.profileURL
                 ? plan.creator.profileURL
-                : `https://ui-avatars.com/api/?name=${plan.creator?.name?.toUpperCase()}&background=0D8ABC&color=fff`
+                : `https://api.dicebear.com/5.x/thumbs/svg?seed=${plan.creator?.name}&scale=70`
             }
             alt="user-img"
             className="user-img"
@@ -51,21 +63,33 @@ const Card: FC<CardProps> = ({ plan, cardType }) => {
           />
         </div>
       </div>
-
-      <img
-        src={plan.planImgURL ?? '/assets/imgs/default-img.png'}
-        width={300}
-        height={200}
-        alt="plan-img"
-      />
+      <Link to={`/detail/${cardType}/${plan._id}`}>
+        <img
+          className="plan-img"
+          src={plan.planImgURL ?? '/assets/imgs/default-img.png'}
+          width={300}
+          alt="plan-img"
+        />
+      </Link>
       <div className="plan-info">
         <h2>{plan.title}</h2>
         <h3>{plan.place}</h3>
         <p>{plan.description}</p>
+        <p>{dateString}</p>
+      </div>
+
+      <div className="plan-info-extra">
+        <h2>Users following this plan: {plan.registeredUsers?.length}</h2>
+        <h2>Status: {plan.status ? 'Public' : 'Private'}</h2>
       </div>
 
       <div className="bottom-label" data-testid="bottom-label">
-        <Button value={'Save'} width={'150px'} styles={'outlined'} />
+        <Button
+          data-testid="save-btn"
+          value={'Save'}
+          width={'150px'}
+          styles={'outlined'}
+        />
         <Button value={'Share'} width={'150px'} styles={'filled'} />
       </div>
     </CardStyled>
