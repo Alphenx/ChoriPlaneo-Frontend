34b7c73c1ef7449a5e6d86c75940676eed9fe0ae
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-29 00:39:59 +0200

    Modify plan model

--- src/features/plans/plan.model.ts
+++ src/features/plans/plan.model.ts
@@ -2,12 +2,13 @@ import User from '../auth/user.model';
 
 type Creator = Pick<User, 'email' | 'name'> & { profileURL?: string };
 export interface Plan {
+  _id: string;
   creator: Creator;
   title: string;
   description: string;
   place: string;
-  status: string;
-  date: Date;
+  status: string | undefined;
+  date: string;
   planImgURL?: string;
-  registeredUsers: User[];
+  registeredUsers: Creator[];
 }
