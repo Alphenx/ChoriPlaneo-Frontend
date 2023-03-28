2d668e62bea32ff1ca6f401d64927110040fca9b
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-29 00:18:20 +0200

    Refactor Header links to match router and Log out

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -7,24 +7,24 @@ import { HeaderStyled } from './HeaderStyled';
 
 const Header = () => {
   const { pathname } = useLocation();
-
   const handleLogout = () => {
     sessionStorage.removeItem('accessToken');
-    sessionStorage.removeItem('userEmail');
   };
   return (
     <HeaderStyled className="header">
       <div className="logo">
-        <img src="/assets/imgs/logo.png" alt="logo" />
+        <Link to={'/home'}>
+          <img src="/assets/imgs/logo.png" alt="logo" />
+        </Link>
         <div className="theme">
           <HandleTheme />
         </div>
       </div>
       <nav>
-        <Link to={'/app'}>
+        <Link to={'/home'}>
           <AiIcons.AiOutlineStar
             data-testid="Plans icon"
-            className={`icon ${pathname === '/app' ? 'selected' : ''}`}
+            className={`icon ${pathname === '/home' ? 'selected' : ''}`}
           />
           <h2>Plans</h2>
         </Link>
@@ -35,10 +35,14 @@ const Header = () => {
           />
           <h2>Social</h2>
         </Link>
-        <Link to={'/app/my-plans'}>
+        <Link to={'/my-plans'}>
           <BsIcons.BsCalendar2CheckFill
             data-testid="My plans icon"
-            className={`icon ${pathname === '/app/my-plans' ? 'selected' : ''}`}
+            className={`icon ${
+              pathname === '/my-plans' || pathname === '/create-plan'
+                ? 'selected'
+                : ''
+            }`}
           />
           <h2>My plans</h2>
         </Link>
@@ -46,7 +50,12 @@ const Header = () => {
           <BsIcons.BsPersonCircle className="icon" />
           <h2>Profile</h2>
         </Link>
-        <Link to={'/'} onClick={() => handleLogout()} data-testid="Log out">
+        <Link
+          to={'/'}
+          onClick={() => handleLogout()}
+          data-testid="Log out"
+          reloadDocument
+        >
           <AiIcons.AiOutlineLogout className="icon" />
           <h2>Log out</h2>
         </Link>
